97f90bac2d016dc86858e2e5e1b5d06d81eb6615
gus <rosalesagustavo@gmail.com>
2025-02-16 19:32:07 -0500

    training the system

--- main.py
+++ main.py
@@ -691,9 +691,9 @@ def collate_batch(
             )
         )
 
-        src = torch.stack(src_list)
-        tgt = torch.stack(tgt_list)
-        return (src, tgt)
+    src = torch.stack(src_list)
+    tgt = torch.stack(tgt_list)
+    return (src, tgt)
 
 def create_dataloaders(
     device,
@@ -752,3 +752,151 @@ def create_dataloaders(
 
     return train_dataloader, valid_dataloader
 
+# Training the System
+
+def train_worker(
+    device,
+    ngpus_per_node,
+    vocab_src,
+    vocab_tgt,
+    spacy_de,
+    spacy_en,
+    config,
+    is_distributed=False,
+):
+    print(f"Train worker process using GPU: {device} for training", flush=True)
+
+    pad_idx = vocab_tgt["<blank>"]
+    d_model = 512
+    model = make_model(len(vocab_src), len(vocab_tgt), N=6)
+    model.to(device) # Instead of .cuda(gpu)
+
+    module = model
+    is_main_process = True
+
+    if is_distributed:
+        dist.init_process_group(
+            backend="nccl" if torch.cuda.is_available() else "gloo",
+            init_method="env://", 
+            rank=0, 
+            world_size=ngpus_per_node
+        )
+        model = DDP(model, device_ids=[device] if device.type == "cuda" else None)
+        module = model.module
+        is_main_process = device == 0
+    
+    criterion = LabelSmoothing(size=len(vocab_tgt), padding_idx=pad_idx, smoothing=0.1).to(device)
+
+    train_dataloader, valid_dataloader = create_dataloaders(
+        device,
+        vocab_src,
+        vocab_tgt,
+        spacy_de,
+        spacy_en,
+        batch_size=config['batch_size'] // ngpus_per_node,
+        max_padding=config['max_padding'],
+        is_distributed=is_distributed,
+    )
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=config['base_lr'], betas=(0.9, 0.98), eps=1e-9
+    )
+    lr_scheduler = LambdaLR(
+        optimizer=optimizer,
+        lr_lambda=lambda step: rate(step, d_model, factor=1, warmup=config["warmup"])
+    )
+    train_state = TrainState()
+
+    for epoch in range(config['num_epochs']):
+        if is_distributed:
+            train_dataloader.sampler.set_epoch(epoch)
+            valid_dataloader.sampler.set_epoch(epoch)
+
+        model.train()
+        print(f"[{device}] Epoch {epoch} Training ====", flush=True)
+
+        _, train_state = run_epoch(
+            (Batch(b[0], b[1], pad_idx) for b in train_dataloader),
+            model,
+            SimpleLossCompute(module.generator, criterion),
+            optimizer,
+            lr_scheduler,
+            mode="train+log",
+            accum_iter=config["accum_iter"],
+            train_state=train_state,
+        )
+        GPUtil.showUtilization() if torch.cuda.is_available() else None
+        
+        if is_main_process:
+            file_path = "%s%.2d.pt" % (config["file_prefix"], epoch)
+            torch.save(module.state_dict(), file_path)
+        torch.cuda.empty_cache()
+
+        print(f"[{device}] Epoch {epoch} Training ====", flush=True)
+        model.eval()
+        sloss = run_epoch(
+            (Batch(b[0], b[1], pad_idx) for b in valid_dataloader),
+            model,
+            SimpleLossCompute(module.generator, criterion),
+            DummyOptimizer(),
+            DummyScheduler(),
+            mode="eval",
+        )
+        print(sloss)
+        torch.cuda.empty_cache()
+
+    if is_main_process:
+        file_path = "%sfinal.pt" % config["file_prefix"]
+        torch.save(module.state_dict(), file_path)
+
+def train_distributed_model(vocab_src, vocab_tgt, spacy_de, spacy_en, config):
+    ngpus = torch.cuda.device_count()
+    os.environ["MASTER_ADDR"] = 'localhost'
+    os.environ["MASTER_PORT"] = '65432'
+    print(f"Number fo GPUs detected: {ngpus}")
+    print("Spawning training processses...")
+    mp.spawn(train_worker, nprocs=ngpus, args=(ngpus, vocab_src, vocab_tgt, spacy_de, spacy_en, config, True))
+
+def train_model(vocab_src, vocab_tgt, spacy_de, spacy_en, config):
+    # Detect if ROCm (AMD GPU) or CUDA (NVIDIA GPU) is available
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0") # NVIDIA GPUs
+    elif torch.version.hip:
+        device = torch.device("cuda:0") # AMD GPUs (ROCm support)
+    else:
+        device = torch.device("cpu") # Fallback to CPU
+
+    if config["distributed"]:
+        train_distributed_model(vocab_src, vocab_tgt, spacy_de, spacy_en, config)
+    else:
+        train_worker(device, 1, vocab_src, vocab_tgt, spacy_de, spacy_en, config, False)
+
+def load_trained_model():
+    config = {
+        "batch_size": 32,
+        "distributed": False,
+        "num_epochs": 8,
+        "accum_iter": 10,
+        "base_lr": 1.0,
+        "max_padding": 72,
+        "warmup": 3000,
+        "file_prefix": "multi30k_model_",
+    }
+    model_path = "multi30k_model_final.pt"
+    if not exists(model_path):
+        train_model(vocab_src, vocab_tgt, spacy_de, spacy_en, config)
+
+    model = make_model(len(vocab_src), len(vocab_tgt), N=6)
+    model.load_state_dict(torch.load(model_path))
+    return model
+
+model = load_trained_model()
+
+# Load Data and model for output checks
+
+# Attention Visualization
+
+# Encoder Self Attention
+
+# Decoder Self Attention
+
+# Decoder Src Attention
