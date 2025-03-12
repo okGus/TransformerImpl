5f62be0cf5ea28bc8e3eb578d021ca5650578b70
gus <rosalesagustavo@gmail.com>
2025-03-12 17:39:01 -0400

    decoder src attention viz

--- main.py
+++ main.py
@@ -1043,6 +1043,19 @@ def viz_decoder_self():
         layer_viz[0] & layer_viz[1] & layer_viz[2] & layer_viz[3] & layer_viz[4] & layer_viz[5]
     )
 
-viz_decoder_self()
+#viz_decoder_self()
 
 # Decoder Src Attention
+def viz_decoder_src():
+    model, example_data = run_model_example(n_examples=1)
+    example = example_data[len(example_data) - 1]
+
+    layer_viz = [
+        visualize_layer(model, layer, get_decoder_src, max(len(example[1]), len(example[2])), example[1], example[1])
+        for layer in range(6)
+    ]
+    return alt.hconcat(
+        layer_viz[0] & layer_viz[1] & layer_viz[2] & layer_viz[3] & layer_viz[4] & layer_viz[5]
+    )
+
+#viz_decoder_src()
