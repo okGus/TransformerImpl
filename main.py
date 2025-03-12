c3a2b475bfceaa19a834748240112544614bb74e
gus <rosalesagustavo@gmail.com>
2025-03-12 17:37:01 -0400

    decoder self attention viz

--- main.py
+++ main.py
@@ -1025,11 +1025,24 @@ def viz_encoder_self():
         for layer in range(6)
     ]
     return alt.hconcat(
-        layer_viz[0] & layer_viz[2] | layer_viz[4]
+        layer_viz[0] & layer_viz[2] & layer_viz[4]
     )
 
 #viz_encoder_self()
 
 # Decoder Self Attention
+def viz_decoder_self():
+    model, example_data = run_model_example(n_examples=1)
+    example = example_data[len(example_data) - 1]
+
+    layer_viz = [
+        visualize_layer(model, layer, get_decoder_self, len(example[1]), example[1], example[1])
+        for layer in range(6)
+    ]
+    return alt.hconcat(
+        layer_viz[0] & layer_viz[1] & layer_viz[2] & layer_viz[3] & layer_viz[4] & layer_viz[5]
+    )
+
+viz_decoder_self()
 
 # Decoder Src Attention
